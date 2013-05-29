648de2635040ddf386c09a277fbcc41e7f6d8c70
Paul Wagener <mail@paulwagener.nl>
2013-05-29 21:44:14 +0200

    Live circle detection

--- vision.py
+++ vision.py
@@ -1,19 +1,18 @@
-import cv2, cv, numpy as np
+import cv2, cv, numpy as np, urllib2
 
 cv2.namedWindow("preview")
-vc = cv2.VideoCapture(0)
 
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
-
+rval = True
 while rval:
     
-    rval, frame = vc.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame = cv2.medianBlur(frame, 3)
-    circles = cv2.HoughCircles(frame, cv2.cv.CV_HOUGH_GRADIENT, 1, 1000, param1=100, param2=30)
+    plaatje = urllib2.urlopen('http://192.168.168.161:8123/snapshot.cgi?user=admin&pwd=31084_robot').read()
+
+    img_array = np.asarray(bytearray(plaatje), dtype=np.uint8)
+    print img_array
+    frame = cv2.imdecode(img_array, 0)
+
+    frame = cv2.medianBlur(frame, 9)
+    circles = cv2.HoughCircles(frame, cv2.cv.CV_HOUGH_GRADIENT, 1, 1000, param1=100, param2=30, minRadius=30, maxRadius=300)
     if circles != None:
         for i in circles[0,:]:
             cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),1)  # draw the outer circle
@@ -25,19 +24,3 @@ while rval:
     if key == 27: # exit on ESC
         break
     
-quit(0)
-img = cv2.imread("ball.jpg", 0)
-
-img = cv2.medianBlur(img, 5)
-#img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-circles = cv2.HoughCircles(img, cv2.cv.CV_HOUGH_GRADIENT, 1, 100, param1=100, param2=30)
-circles = np.uint16(np.around(circles))
-print circles
-for i in circles[0,:]:
-    cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),1)  # draw the outer circle
-    cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)     # draw the center of the circle
-
-
-cv2.imshow('detected circles',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
